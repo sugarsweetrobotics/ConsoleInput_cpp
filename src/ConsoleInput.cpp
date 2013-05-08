1bcf6ab643a88af1cdce890f51b460d8af368d2a
Yuki Suga <ysuga@ysuga.net>
2013-05-08 15:47:27 +0900

    This if first commit. This repository is generated by wasanbon

---
+++ src/ConsoleInput.cpp
@@ -0,0 +1,165 @@
+// -*- C++ -*-
+/*!
+ * @file  ConsoleInput.cpp
+ * @brief Console Input RT component
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "ConsoleInput.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* consoleinput_spec[] =
+  {
+    "implementation_id", "ConsoleInput",
+    "type_name",         "ConsoleInput",
+    "description",       "Console Input RT component",
+    "version",           "1.0.0",
+    "vendor",            "Sugar Sweet Robotics",
+    "category",          "Tutorial",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+ConsoleInput::ConsoleInput(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_outOut("out", m_out)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+ConsoleInput::~ConsoleInput()
+{
+}
+
+
+
+RTC::ReturnCode_t ConsoleInput::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  
+  // Set OutPort buffer
+  addOutPort("out", m_outOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t ConsoleInput::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ConsoleInput::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ConsoleInput::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t ConsoleInput::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ConsoleInput::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t ConsoleInput::onExecute(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t ConsoleInput::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ConsoleInput::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ConsoleInput::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ConsoleInput::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t ConsoleInput::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void ConsoleInputInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(consoleinput_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<ConsoleInput>,
+                             RTC::Delete<ConsoleInput>);
+  }
+  
+};
+
+
